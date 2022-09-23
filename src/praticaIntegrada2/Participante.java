3a277a0d8ed21eff75ab2035b2721b5ac55cfb4a
ranisantos <raniel.santos@mercadolivre.com>
2022-09-23 11:19:58 -0300

    Melhora codigo adicionando a classe Pessoa

--- src/praticaIntegrada2/Participante.java
+++ src/praticaIntegrada2/Participante.java
@@ -1,14 +1,6 @@
 package praticaIntegrada2;
 
-public class Participante {
-    //atributos
-    private String nome;
-    private String sobrenome;
-    private String rg;
-    private int idade;
-    private String celular;
-    private String celEmergencia;
-    private String tipoSanguineo;
+public class Participante extends  Pessoa{
     private int circuito;
     private int valorIncricao;
 
@@ -37,62 +29,6 @@ public class Participante {
 
     public void exibirNumeroNome(int index) {
         System.out.println("Participante " + getNome() + " Numero: " + index);
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
-
-    public String getRg() {
-        return rg;
-    }
-
-    public void setRg(String rg) {
-        this.rg = rg;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getCelular() {
-        return celular;
-    }
-
-    public void setCelular(String celular) {
-        this.celular = celular;
-    }
-
-    public String getCelEmergencia() {
-        return celEmergencia;
-    }
-
-    public void setCelEmergencia(String celEmergencia) {
-        this.celEmergencia = celEmergencia;
-    }
-
-    public String getTipoSanguineo() {
-        return tipoSanguineo;
-    }
-
-    public void setTipoSanguineo(String tipoSanguineo) {
-        this.tipoSanguineo = tipoSanguineo;
     }
 
     public int getCircuito() {
